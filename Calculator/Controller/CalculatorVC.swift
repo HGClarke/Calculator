d28659123a9844c016d657f09ec73d7376207399
Holland Clarke <hollandclarke@Hollands-MacBook-Pro.local>
2019-02-28 13:29:29 -0500

    Added Functions and Operations

--- Calculator/Controller/CalculatorVC.swift
+++ Calculator/Controller/CalculatorVC.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalculatorVC: UIViewController {
+    
+    let calculator = Calculator()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
 
